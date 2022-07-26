483eebe28fea6241983d75be3cf6c9635dff204c
xiaoqinxing <1415289814@qq.com>
2022-07-26 21:32:23 +0800

    ADD: cv版本

--- buildexe.bat
+++ buildexe.bat
@@ -3,4 +3,5 @@ DEL /q .\dist\*
 RD /S /Q .\build
 RD /S /Q .\dist
 pip install pyinstaller
-pyinstaller -w ./ImageViewer.py --noconfirm && compil32 /cc ./ImageViewer.iss
+pyinstaller -w ./ImageViewer.py --noconfirm
+compil32 /cc ./ImageViewer.iss
